d884a3c55d157716d6b42164170de7a68826aa1c
shasini274 <shasiniumesha24@gmail.com>
2020-06-25 01:18:15 +0530

    Minor CHnages

--- run_utils.sh
+++ run_utils.sh
@@ -7,8 +7,8 @@ echo `date`
 # Change directory into where email_notifer.py script is located.
 echo "Changing into ~/curw_iot_email_notifer"
 
-# shellcheck disable=SC2164
-#cd /home/uwcc-admin/curw_iot_email_notifer
+
+cd /home/uwcc-admin/curw_iot_email_notifer/
 echo "Inside `pwd`"
 
 # If no venv (python3 virtual environment) exists, then create one.
